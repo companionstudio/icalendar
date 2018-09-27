6208349ba0f2bca526fc32a2246e2e86484edb50
Ben <ben@spookandpuff.com>
2018-09-27 14:04:46 +0930

    Use a bare value for CalAddress for now (was URI)

--- lib/icalendar/values/cal_address.rb
+++ lib/icalendar/values/cal_address.rb
@@ -1,7 +1,16 @@
 module Icalendar
   module Values
 
-    class CalAddress < Uri
+    class CalAddress < Value
+
+      def initialize(value, params = {})
+        parsed = URI.parse(value) rescue value
+        super parsed, params
+      end
+
+      def value_ical
+        value.to_s
+      end
     end
 
   end
